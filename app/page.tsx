40de9ca17953a68f8a6f9c63c2df66aa06288eb6
Krishanu bera <berakrishanu473@gmail.com>
2025-12-29 17:50:59 +0530

    Revert "pushed"
    This reverts commit 0c4d23c55be1373576cff943c34e7a0de54caf02.

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import {Button} from "@/components/ui/button";
 
 const Home = () => {
     return (
-        <div className="flex min-h-screen home-wrapper">
-            Home
+        <div className="flex justify-center items-center h-screen">
+            <Button>Click me</Button>
         </div>
     )
 }
 export default Home
+
